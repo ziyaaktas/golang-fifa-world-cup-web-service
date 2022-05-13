29c5a7a13b270b65082afc6561d34331cab6b753
zaktas <ziya.aktas@factset.com>
2022-05-13 23:11:00 +0200

    Part 3

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -33,7 +33,18 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
-
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	isTokenValid := data.IsAccessTokenValid(accessToken)
+	if(!isTokenValid) {
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		err := data.AddNewWinner(req.Body)
+		if(err != nil) {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
+	}
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
